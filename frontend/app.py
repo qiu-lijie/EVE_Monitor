4ad53393b5d2b9efb50caf4677f370c9cd85b109
qiu_lijie <qiu_lijie@ymail.com>
2026-01-22 15:50:32 -0800

    on connect/reconnect, UI deletes existing logs and refreshes last 500 lines, keeping only 1000 lines on screen

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,18 @@
+import logging
 import select
 import subprocess
 import threading
-import time
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 
-LOG_FILE = "logs/temp.log"
+LOG_FILE = "logs/tasks.log"
+UPDATE = "update"
+SEND_LAST_LINES = 500
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+logger = logging.getLogger(__name__)
 
 
 @app.route("/")
@@ -18,9 +21,23 @@ def index():
 
 
 @socketio.on("connect")
-def handle_message():
-    print("Client connected")
-    emit("update", "Connected")
+def handle_connect():
+    logger.info("client connected")
+    lines = tail(LOG_FILE, SEND_LAST_LINES)
+    res = "Connected\n"
+    for line in lines:
+        res += str(line, "utf-8")
+    emit(UPDATE, (res, True))
+    return
+
+
+def tail(file: str, n: int) -> list[bytes]:
+    """return last n lines from file f with unix tail"""
+    proc = subprocess.Popen(["tail", "-n", str(n), file], stdout=subprocess.PIPE)
+    if proc.stdout == None:  # only to suppress type warnings
+        raise Exception("unexpected error")
+    lines = proc.stdout.readlines()
+    return lines
 
 
 def update():
@@ -38,7 +55,7 @@ def update():
         if p.poll(1):
             line = str(f.stdout.readline(), "utf-8")
             if line != "":
-                socketio.emit("update", line)
+                socketio.emit(UPDATE, line)
     return
 
 
